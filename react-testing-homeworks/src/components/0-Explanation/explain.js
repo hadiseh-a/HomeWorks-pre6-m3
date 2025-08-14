0a1df6c579813bf168990f6b4005a4d652b8e16c
hadiseh-a <hadisea82@gmail.com>
2025-08-14 11:58:11 +0330

    add

---
+++ react-testing-homeworks/src/components/0-Explanation/explain.js
@@ -0,0 +1,4 @@
+// explain the difference between getBy, findBy, queryBy:
+// get by is for normal getting data and if had no data it will throw an error
+// in find by we use it for asyncrounes part of component like fetching data or somthing like that takes time to rerender the component
+// the only different between get and query is inestead of error this query if didn't find that element it going to return null
